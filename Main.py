34bd62737c967adc22734e0a61b9d656c8cec32d
Ayush-sharma <57178732+Ayush-sharma-py@users.noreply.github.com>
2020-09-05 19:13:46 +0530

    Commit 4
    Content Filter

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ import pytesseract
 
 #Directory to where training images are kept
 training_directory = "C:\\Users\\Ayush Sharma\\Desktop\\Programs\\HTML-Extractor\\Data"
+#Configuring Tesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 training_files = os.listdir(training_directory)
 
@@ -18,6 +20,24 @@ for image_directory in training_files:
 
 #html_tags = ["<p></p>"]
 
+#Function for extracting text from image 
+def extract_text(PIL_open):
+    return pytesseract.image_to_string(PIL_open)
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-print(pytesseract.image_to_string(training_images[2]))
+print(extract_text(training_images[3]).split("\n\n"))
+
+#Creating the html file
+def create_html_doc(name,PIL_open):
+    main_tags = ["<html>","<head>","<title>","<body>"]
+    additional_tag = ["<p>"]
+
+    with open(name + ".html","a+") as f:
+        f.write("<html>")
+
+
+
+
+        f.write("</html>")
+        f.close()
+
+    
