c72f0a4347a58f2504e078e26ddc3ed1a140547a
Ayush-sharma <57178732+Ayush-sharma-py@users.noreply.github.com>
2020-09-05 14:40:28 +0530

    Commit 2
    Images are being read and converted into numpy arrays

--- Main.py
+++ Main.py
@@ -1,5 +1,17 @@
 import os
 import numpy as np
 import PIL
+import PIL.Image
 
-directory = ""
+#Directory to where training images are kept
+training_directory = "C:\\Users\\Ayush Sharma\\Desktop\\Programs\\HTML-Extractor\\Data"
+
+training_files = os.listdir(training_directory)
+
+#List of training images converted into numpy arrays
+training_images = []
+
+#Iterating through to add images in array format to the training_images list
+for image_directory in training_files:
+    training_images.append(np.array(PIL.Image.open(training_directory + "\\" + image_directory)))
+
