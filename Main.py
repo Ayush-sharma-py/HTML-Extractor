6f49ec3a2628e4303bff2692b2176b2dd3b1f216
Ayush-sharma <57178732+Ayush-sharma-py@users.noreply.github.com>
2020-09-05 18:08:13 +0530

    Commit 3
    Tessaract is working OCR reading complete

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import PIL
 import PIL.Image
+import pytesseract
 
 #Directory to where training images are kept
 training_directory = "C:\\Users\\Ayush Sharma\\Desktop\\Programs\\HTML-Extractor\\Data"
@@ -13,5 +14,10 @@ training_images = []
 
 #Iterating through to add images in array format to the training_images list
 for image_directory in training_files:
-    training_images.append(np.array(PIL.Image.open(training_directory + "\\" + image_directory)))
+    training_images.append(PIL.Image.open(training_directory + "\\" + image_directory))
 
+#html_tags = ["<p></p>"]
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+print(pytesseract.image_to_string(training_images[2]))
