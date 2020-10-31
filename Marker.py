c5e3b3adffc7f8016a9dbb2ae476576a670d6abc
Ayush-sharma <57178732+Ayush-sharma-py@users.noreply.github.com>
2020-10-31 21:51:27 +0530

    Commit 9
    Function to calculate seperation_coefficient

--- Marker.py
+++ Marker.py
@@ -1,13 +1,16 @@
 import os
-import tensorflow
+#import tensorflow
+#from tensorflow import keras
 import numpy
 import matplotlib.image as img
 import PIL
-from PIL import Image,ImageDraw2
-from tensorflow import keras
+from PIL import Image,ImageDraw2,ImageOps
+from image_slicer import slice
+
 
 input_training_directory = "C:\\Users\\Ayush Sharma\\Desktop\\Programs\\HTML-Extractor\\Data"
 resized_image_training_directory = input_training_directory + "\\" + "Resized"
+
 try:
     os.mkdir(resized_image_training_directory)
 except:
@@ -16,6 +19,7 @@ except:
 for directory in os.listdir(input_training_directory):
     try:
         n = Image.open(input_training_directory+ "\\" + directory)
+        n = ImageOps.grayscale(n)
         n = n.resize((1500,1500))
         n.save(resized_image_training_directory + "\\" + directory)
     except PermissionError:
@@ -26,10 +30,19 @@ for directory in os.listdir(resized_image_training_directory):
     n = img.imread(resized_image_training_directory + "\\" + directory)
     images.append(n)
 
-training_image_array = numpy.array(images )
+training_image_array = numpy.array(images)
 print(training_image_array.shape)
 
+def calculate_seperation_coefficient(directory):
+    seperation_coefficient = 1
+    image = PIL.Image.open(directory)
+    width,height = image.size
+    seperation_coefficient = round(width/25)
+    return seperation_coefficient
+
+slice(input_training_directory + "\\" + "Html1.png",calculate_seperation_coefficient(input_training_directory + "\\" + "Html1.png"))
+
+slice(resized_image_training_directory + "\\Html1.png" , 4)
 
 
-
-
+#output - array[list[tuples(x,y)]]
